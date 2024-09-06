b37f3ba15a3615e0c9007441ba8db2cfc8e57615
jjiiyun <jiyunkim.gm@gmail.com>
2024-09-06 17:37:41 +0900

    security

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+// production. 배포 후
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const express = require('express')
+// import express from 'express';
+const express = require('express');
 const app = express()
 const port = 5000
+const { User } = require('./models/User');
+// import User from './models/User.js';
+
+const config = require('./config/key')
+
+//body-parser 은 이제 express에 포함이 됨
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+//for json request
+app.use(express.json());
+
+
+// import mongoose from 'mongoose';
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jiyun:3257@boilerplate.wa5um.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate')
+mongoose.connect(config.mongoURI)
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    // 회원 가입할 때 필요한 정보들을 client 에서 가져오면 그것들을 db 에 넣어준다.
+    const user = new User(req.body);
+    //mongoDB 의 함수
+    user.save()
+        .then((userInfo) => res.status(200).json({ success: true }))
+        .catch((err) => res.json({ success: false, err }));
 })
 
 app.listen(port, () => {
